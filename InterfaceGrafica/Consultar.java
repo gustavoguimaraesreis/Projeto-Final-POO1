343bc62c9a3aecdaf556d0cb3f6c464465d8b706
gcbsamaral <80076656+gcbsamaral@users.noreply.github.com>
2021-10-24 20:39:59 -0300

    Correcoes "Consultar" - GUI

--- InterfaceGrafica/Consultar.java
+++ InterfaceGrafica/Consultar.java
@@ -1,5 +1,4 @@
 package InterfaceGrafica;
-
 import javax.swing.*;
 import java.awt.*;
 import javax.swing.border.EmptyBorder;
@@ -7,61 +6,42 @@ import javax.swing.border.EmptyBorder;
 public class Consultar extends JFrame {
 
 	private JPanel contentPane;
-	private JButton btnPacienteComPlano, btnPacienteSemPlano, btnMedicos, btnOutroFuncionario, btnServiçoExtra, btnVoltar;
+	private JButton btnPaciente, btnFuncionario, btnServiçoExtra, btnConsulta, btnVoltar;
 	
 	public Consultar() {
 		super("Consulta");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 700, 500);
+		setBounds(100, 100, 700, 320);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblOQueDeseja = new JLabel("O que deseja consultar?");
+		JLabel lblOQueDeseja = new JLabel("O que deseja consultar no sistema?");
 		lblOQueDeseja.setFont(new Font("Dialog", Font.BOLD, 22));
-		lblOQueDeseja.setBounds(200, 24, 369, 15);
+		lblOQueDeseja.setBounds(122, 25, 457, 27);
 		contentPane.add(lblOQueDeseja);
 		
-		JLabel lblPacientes = new JLabel("Pacientes");
-		lblPacientes.setFont(new Font("Dialog", Font.BOLD, 18));
-		lblPacientes.setBounds(70, 119, 151, 15);
-		contentPane.add(lblPacientes);
+		btnPaciente = new JButton("Paciente");
+		btnPaciente.setBounds(44, 113, 180, 25);
+		contentPane.add(btnPaciente);
 		
-		JLabel lblFuncionarios = new JLabel("Funcionarios");
-		lblFuncionarios.setFont(new Font("Dialog", Font.BOLD, 18));
-		lblFuncionarios.setBounds(294, 118, 171, 15);
-		contentPane.add(lblFuncionarios);
-		
-		JLabel lblServiosExtras = new JLabel("Serviços Extras");
-		lblServiosExtras.setFont(new Font("Dialog", Font.BOLD, 18));
-		lblServiosExtras.setBounds(497, 119, 168, 15);
-		contentPane.add(lblServiosExtras);
-		
-		btnPacienteComPlano = new JButton("Paciente Com Plano");
-		btnPacienteComPlano.setBounds(41, 176, 180, 25);
-		contentPane.add(btnPacienteComPlano);
-		
-		btnPacienteSemPlano = new JButton("Paciente Sem Plano");
-		btnPacienteSemPlano.setBounds(41, 258, 180, 25);
-		contentPane.add(btnPacienteSemPlano);
-		
-		btnMedicos = new JButton("Médico");
-		btnMedicos.setBounds(306, 176, 117, 25);
-		contentPane.add(btnMedicos);
-		
-		btnOutroFuncionario = new JButton("Outro Funcionario");
-		btnOutroFuncionario.setBounds(285, 258, 162, 25);
-		contentPane.add(btnOutroFuncionario);
+		btnFuncionario = new JButton("Funcionario");
+		btnFuncionario.setBounds(199, 156, 173, 25);
+		contentPane.add(btnFuncionario);
 		
 		btnServiçoExtra = new JButton("Serviço Extra");
-		btnServiçoExtra.setBounds(503, 176, 162, 25);
+		btnServiçoExtra.setBounds(490, 244, 162, 25);
 		contentPane.add(btnServiçoExtra);
+
+		btnConsulta = new JButton("Consulta");
+		btnConsulta.setBounds(349, 201, 162, 25);
+		contentPane.add(btnConsulta);
 		
 		btnVoltar = new JButton("Voltar");
 		btnVoltar.setBackground(Color.RED);
 		btnVoltar.setForeground(Color.WHITE);
-		btnVoltar.setBounds(554, 426, 117, 25);
+		btnVoltar.setBounds(12, 246, 117, 25);
 		contentPane.add(btnVoltar);
-	}
+	}		
 }
